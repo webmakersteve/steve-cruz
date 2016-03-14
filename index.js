21db6f1b5bf5a6c6ef90e7ae44bdeb76e6b2fc13
Stephen Parente <webmakersteve@gmail.com>
2016-03-13 23:32:12 -0700

    Commit of work

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var morgan = require('morgan');
+var compression = require('compression');
+var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var mustacheExpress = require('mustache-express');
+
+var path = require('path');
+
+var app = express();
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'html');
+app.engine('html', mustacheExpress());
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(compression());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.set('appPath', path.join(__dirname, 'public'));
+
+app.use('/sentiment', require('./controllers/sentiment.controller'));
+app.use('/', require('./controllers/index.controller'));
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
